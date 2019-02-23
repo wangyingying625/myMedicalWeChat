db7417f1c552baa6620a19b99c043f44dd1fd802
mldwyy <1224374496@qq.com>
2019-02-23 21:09:53 +0800

    add change

--- miniprogram/pages/change/change.js
+++ miniprogram/pages/change/change.js
@@ -5,14 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    avatarUrl:'',
+    nickName:''
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    that.setData({
+      nickName: app.globalData.userInfo.nickName,
+      avatarUrl: app.globalData.userInfo.avatarUrl,
+    })
   },
 
   /**
